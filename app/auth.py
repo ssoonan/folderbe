3ce1c5bb46a36b55da9c2050c2dade67e3f9feba
ssoonan <ssoonan0770@gmail.com>
2022-08-18 10:38:27 +0900

    Chor: session 영구적으로 설정

--- app/auth.py
+++ app/auth.py
@@ -57,6 +57,7 @@ def callback():
               "redirect_uri": url_for("auth.callback", _external=True),
               "grant_type": "authorization_code"}
     response = requests.post(TOKEN_URL, params=params).json()
+    session.permanent = True
     session['expired_at'] = time.time() + response['expires_in']  # 토큰 만료시간 기입
     session['access_token'] = response['access_token']
     session['refresh_token'] = response['refresh_token']
@@ -91,6 +92,7 @@ def refresh_token():
     response = requests.post(TOKEN_URL, params=params)
     if response.status_code != 200:
         return redirect(url_for("auth.authorize")) # refresh token도 만료 되면 재인증을 거쳐야함. 
+    session.permanent = True
     session['access_token'] = response.json()['access_token']  # 이 때 id_token도 같이 오긴 하네
     session['expired_at'] = time.time() + response.json()['expires_in']  # 토큰 만료시간 기입
     return redirect(url_for("main.index"))
